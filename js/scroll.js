4e2e7b28edb104659ed285c26ebd97a502924a42
euichan0927 <yuichan99@naver.com>
2024-01-22 17:43:28 +0900

    FEproject 이미지 무한로드 수정 commit

--- js/scroll.js
+++ js/scroll.js
@@ -4,38 +4,29 @@
     let fetching = false;
 
     let totalImagesLoaded = 0; // 로드된 이미지의 총 개수를 추적하는 변수
-    const maxImages = 10000000000000; // 최대 이미지 로드 개수
+
 
     async function fetchImages(page) {
-    // 이미 최대 개수에 도달했다면 추가 로드하지 않음
-        if (totalImagesLoaded >= maxImages) {
-            window.removeEventListener('scroll', handleScroll);
-            return;
-    }
 
-        if (fetching) return;
-        fetching = true;
 
         try {
             const response = await fetch(`https://picsum.photos/v2/list?page=${page}&limit=3`);
             if (!response.ok) {
                 throw new Error('네트워크 응답에 문제가 있습니다.');
-                }
+            }
 
-        const images = await response.json();
-        makeImageList(images);
-        totalImagesLoaded += images.length; // 로드된 이미지의 개수를 업데이트
+            const images = await response.json();
+            makeImageList(images);
+            totalImagesLoaded += images.length; // 로드된 이미지의 개수를 업데이트
 
-        // 최대 개수에 도달했다면 스크롤 이벤트 리스너를 제거
-        if (totalImagesLoaded >= maxImages) {
-            window.removeEventListener('scroll', handleScroll);
-}
+        }
 
-    fetching = false;
-} catch (error) {
-    console.error('데이터를 가져오는데 문제가 발생했습니다:', error);
-    fetching = false;
-}
+
+
+        catch (error) {
+            console.error('데이터를 가져오는데 문제가 발생했습니다:', error);
+            fetching = false;
+        }
 }
 
     function makeImageList(images) {
@@ -55,7 +46,7 @@
             imageRow.appendChild(img);
 });
 
-    imgWrapper.appendChild(imageRow); // 마지막 라인을 추가합니다.
+    imgWrapper.appendChild(imageRow); // 마지막 라인을 추가
 }
 
     const showMoreBtn = document.querySelector(".more_but button");
